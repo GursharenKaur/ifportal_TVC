b790678d8581727bd69117dde024b6859787a891
Gursharen Kaur Suri <150019277+GursharenKaur@users.noreply.github.com>
2026-02-17 12:39:20 +0530

    Update studentController.js

--- backend/src/controllers/studentController.js
+++ backend/src/controllers/studentController.js
@@ -1,3 +1,10 @@
+const cloudinary = require('cloudinary').v2;
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
 const asyncHandler = require('express-async-handler');
 const Company = require('../models/Company');
 const Role = require('../models/Role');
